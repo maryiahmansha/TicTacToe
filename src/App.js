6eebfefcce81612c9dfaca414395932559e30d91
Mary1-5 <marymansha15@gmail.com>
2023-09-21 15:41:05 +0100

    implementing status

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true)
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -37,8 +37,17 @@ export default function Board() {
     setSquares(nextSquares)
     setXIsNext(!xIsNext)
   }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
